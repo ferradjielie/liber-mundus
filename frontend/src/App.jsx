e2ab60a709a3eaf102217815f7f4cbef8071f330
ferradjielie <ferradji.elie@outlook.fr>
2025-11-02 17:58:00 +0100

    feat: set up React Router with routes for Home and Countries pages
    - Installed and configured react-router-dom
    - Added <BrowserRouter> wrapper in index.jsx
    - Defined routes for Home ("/") and Countries ("/Liste-des-pays") in App.jsx
    - Integrated Nav and Footer components to persist across all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+
+import {Routes, Route, Link} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Nav from "./components/Nav/Nav";
 import Footer from "./components/Footer/Footer";
@@ -7,11 +9,19 @@ import Countries from "./pages/Countries/Countries";
 function App() {
   return (
     <>
-      <Nav/>
-      <Home />
-      <Footer/>
-      <Countries/>
+     <Nav/>
+    <Routes>
+      
+     
 
+     <Route path="/" element={<Home />} />
+        <Route path="/liste-des-pays" element={<Countries/> }   />
+
+    </Routes>
+    
+     
+      <Footer/>
+     
     </>
   );
 }
