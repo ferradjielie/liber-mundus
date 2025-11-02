5e92aa40609698deda6b755fd9c97a1459d5d9c8
ferradjielie <ferradji.elie@outlook.fr>
2025-11-02 19:38:01 +0100

    Set up AuthorsList page structure
    - Created AuthorsList folder with AuthorsList.jsx and AuthorsList.module.css
    - Imported useState, useEffect, and useParams in AuthorsList.jsx
    - Prepared component for future dynamic data fetching and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 
 import {Routes, Route, Link} from "react-router-dom";
+
 import Home from "./pages/Home/Home";
 import Nav from "./components/Nav/Nav";
 import Footer from "./components/Footer/Footer";
 import Countries from "./pages/Countries/Countries";
+import AuthorsList from "./pages/AuthorsList/AuthorsList";
 
 
 function App() {
@@ -15,7 +17,9 @@ function App() {
      
 
      <Route path="/" element={<Home />} />
-        <Route path="/liste-des-pays" element={<Countries/> }   />
+     <Route path="/liste-des-pays" element={<Countries/> }   />
+     <Route path="/pays/:id/auteurs" element={<AuthorsList />} />
+
 
     </Routes>
     
---
+++ frontend/src/pages/AuthorsList/AuthorsList.jsx
@@ -0,0 +1,9 @@
+import styles from './AuthorsList.module.css';
+import { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+
+function AuthorsList() {
+
+}
+
+export default AuthorsList
